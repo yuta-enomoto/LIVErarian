d2382cf64420737523b51caf96ccd9aa5c62cfa2
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-07 19:50:27 +0900

    誕生日カラムを追加

--- db/migrate/20200305063849_devise_create_users.rb
+++ db/migrate/20200305063849_devise_create_users.rb
@@ -5,6 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       ## Database authenticatable
       t.string :nickname,           null: false
+      t.date :birthday,             null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
