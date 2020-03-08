9737dbf27dee93f2bb43613a6908bba612c552da
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-08 11:42:22 +0900

    artistsのマイグレーションファイル、modelを作成

--- db/migrate/20200308024115_create_artists.rb
+++ db/migrate/20200308024115_create_artists.rb
@@ -6,7 +6,7 @@ class CreateArtists < ActiveRecord::Migration[5.2]
       t.string :active_place,           null: false
       t.text :introduction,             null: false
       t.references :user,               null: false, foreign_key: true
-
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_021130) do
+ActiveRecord::Schema.define(version: 2020_03_08_024115) do
 
   create_table "artists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_03_08_021130) do
     t.string "active_place", null: false
     t.text "introduction", null: false
     t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_artists_on_user_id"
   end
 
