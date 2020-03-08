3969af013b110c42fd2fe881c01284310e14dfdf
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-08 17:10:03 +0900

    差し替え機能を追加

--- app/assets/javascripts/artist-image.js
+++ app/assets/javascripts/artist-image.js
@@ -6,13 +6,13 @@ $(document).on('turbolinks:load', function(){
                     <div class="img-box">
                       <img src="" alt="preview">
                     </div>
-                    <div class="upd-box", id ="upd-btn">
-                      <span class="upd-btn">画像を変更</span>
+                    <div class="upd-box"">
+                      <span class="upd-btn",id ="upd-btn">画像を変更</span>
                     </div>
                   </div>`
       return html;
     }
-    
+    //画像を選択した時=====================================================
     $(document).on('change', '.img-form', function() {
       var file = this.files[0];
       var reader = new FileReader();
@@ -20,15 +20,23 @@ $(document).on('turbolinks:load', function(){
 
       reader.onload = function() {
         var image = this.result;
-        var html = buildHTML();
-        $('.rgst-artist__container__field__label').before(html);
-        //画像を読み込ませる==========================================
         $(`#preview-box img`).attr('src', `${image}`);
-        //ファイルフィールドを隠す=====================================
-        $('.rgst-artist__container__field__label').hide();
-        $('.rgst-artist__container__field__select').hide();
 
+        if ($(`#preview-box`).length == 0) {
+          var html = buildHTML();
+          $('.rgst-artist__container__field__label').before(html);
+          //画像を読み込ませる====================
+          $(`#preview-box img`).attr('src', `${image}`);
+          //ファイルフィールドを隠す=================
+          $('.rgst-artist__container__field__label').hide();
+          $('.rgst-artist__container__field__select').hide();
+        }
       }
     });
+
+    //画像を差し替える時====================================================
+    $(document).on('click', '.upd-btn', function() {
+      $(`input#img`).trigger('click');
+    })
   });
 });
