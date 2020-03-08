1d2e4793c37229babbd08ca6e43a2d39f9e844eb
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-08 11:49:17 +0900

    ルーティングを設定

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,15 @@
 class ArtistsController < ApplicationController
+
   def new
+    @artist = Artist.new
   end
+
+  def create 
+    if @artist.save
+      redirect_to user_path
+    else
+      render :new
+    end
+  end
+
 end
