2e79b10aed1724527545b278518d2f0a13233b3d
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-09 23:44:07 +0900

    ビューを作成1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
   belongs_to :artist
+  belongs_to_active_hash :form
+  belongs_to_active_hash :how_long
 
 end
