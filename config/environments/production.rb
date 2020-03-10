73476da42f3487f63e98b3e84483d8a8f4aa25ba
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-10 13:26:36 +0900

    Revert "自動deploy"
    This reverts commit c02fa797d65f1f2569267db3473e9214c0d7669a.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  server '54.65.243.179', user: 'ec2-user', roles: %w{app db web}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
