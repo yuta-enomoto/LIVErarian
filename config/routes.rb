e1e6141b59dc69e80e7386d9a0d032c431093391
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-12 03:59:23 +0900

    削除後のページ遷移を分岐

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :posts, except: [:index] do
     member do
       get 'done_show'
+      delete 'done_destroy'
     end
     collection do
       get 'live_yet'
