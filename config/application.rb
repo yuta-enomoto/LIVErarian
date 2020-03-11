ed43470826d19c8a89bac30584d55caf5b8a4e0e
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-11 19:25:53 +0900

    時間になったら消えるという記述を修正

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module LIVErarian
     config.i18n.default_locale = :ja
     config.generators do |g|
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local 
       g.stylesheets false
       g.javascripts false
       g.helper false
