e88cd37e6063450b1a819876d8ac4b683549c94c
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-12 22:28:08 +0900

    擬似的な自動機能を全体に反映

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,31 @@
 class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-
+  before_action :status_change
 
   private
+
   def configre_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday])
   end
+
+
+  def status_change 
+    @status = Post.order(date_time: "ASC").find_by(status: '1')
+    @time = DateTime.now.to_s(:db)
+    if @status.present?
+      if Rails.env.production?
+        if @status.date_time < @time
+          @status.status = '0'
+          @status.save
+        end
+      else
+        if @status.date_time + 9.hour < @time
+          @status.status = '0'
+          @status.save
+        end
+      end
+    end
+  end
+
 end
