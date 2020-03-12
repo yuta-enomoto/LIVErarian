cdacd41e4037a1def734950bb23d26619ebc2fe5
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-12 21:51:56 +0900

    ライブハウスの一覧を表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
     @news = Artist.all.order(id: "DESC")
     @day = Date.today.to_s(:db)
     @todays = Post.where(date_time: Time.zone.now.all_day ,status: '1', form_id: '2').order(date_time: "ASC")
+    @today_houses = Post.where(date_time: Time.zone.now.all_day ,status: '1', form_id: '1').order(date_time: "ASC")
     if user_signed_in?
       @artist_info = current_user.artist
     end
