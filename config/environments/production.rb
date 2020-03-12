b53ca05cc84f358dd21897f935db88176dac3da0
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-12 23:48:55 +0900

    googleアナリティクス

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  GA.tracker = "UA-145052855-1"
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
