2d2204949dd256baa9635f20ce75331c2845af3a
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-15 04:31:38 +0900

    修正

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -2,8 +2,8 @@ $(document).on('turbolinks:load', function(){
   $(function(){
     $(".slider").not('.slick-initialized').slick({
       autoplay: true,
-      autoplaySpeed: 4000,
-      speed: 800,
+      autoplaySpeed: 3500,
+      speed: 850,
       pauseOnHover: false,
       prevArrow: '<i class="fas fa-chevron-left left-arrow"></i>',
       nextArrow: '<i class="fas fa-chevron-right right-arrow"></i>',
