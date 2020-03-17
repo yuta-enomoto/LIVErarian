6fb29c5cda42013e8d58761434596b2cd463b230
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-18 00:45:10 +0900

    コントローラーを作成、ルーティング

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,26 @@
+class LikesController < ApplicationController
+  before_action :set_like, only: [:create, :destroy]
+
+  # def index
+  #   @posts = Like.where(user_id: current_user.id)
+  # end
+
+  def create
+    @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
+  end
+
+
+  def destroy
+    @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+    @like.destroy
+  end
+
+  
+  private
+
+  def set_like
+    @post = Post.find(params[:post_id])
+  end
+
+
+end
