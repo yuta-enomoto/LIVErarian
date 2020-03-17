12249d92dd1ffc868276002ebba4a6181b5e4f0a
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-17 14:30:37 +0900

    いいねを仮で実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,12 @@ class FavoritesController < ApplicationController
   before_action :set_favorite
 
   def create
-    @favorite = Like.create(user_id: current_user.id, artist_id: params[:artist_id])
+    @favorite = Favorite.create(user_id: current_user.id, artist_id: params[:artist_id])
   end
 
 
   def destroy
-    @favorite = Like.find_by(user_id: current_user.id, artist_id: params[:artist_id])
+    @favorite = Favorite.find_by(user_id: current_user.id, artist_id: params[:artist_id])
     @favorite.destroy
   end
 
