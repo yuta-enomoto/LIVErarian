42de1cafd1681a2e7d88c9a5e9514056631975e0
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-20 00:52:02 +0900

    駅カラムから表示（test）

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,18 +3,41 @@ function initMap() {
 
   var target = document.getElementById('map');
   var map;
-  var tokyo = {lat: gon.latitude, lng: gon.longitude};
-  var marker;
+  var geocoder = new google.maps.Geocoder();
 
-  map = new google.maps.Map(target, {
-    center: tokyo,
-    zoom: 15
-  });
 
-  marker = new google.maps.Marker({
-    position: tokyo,
-    map: map,
-    title: 'Tokyo!',
-    animation: google.maps.Animation.DROP
-  });
+
+  if (window.location.href.match(/\/posts\/\d+\/show_post/)){
+    geocoder.geocode({
+      address: gon.address
+    }, function(results, status) {
+      if (status !== 'OK') {
+        alert('Failed: ' + status);
+        return;
+      }
+
+  
+      if (results[0]) {
+        new google.maps.Map(target, {
+          center: results[0].geometry.location,
+          zoom: 15
+        });
+        // new google.maps.Marker(target,{
+        //   position: results[0].geometry.location,
+        //   map: map,
+        //   title: 'Tokyo!',
+        //   animation: google.maps.Animation.DROP
+        // });
+      } else {
+        alert('No results found');
+        return;
+      }
+    });
+  };
+
+
+  // map = new google.maps.Map(target, {
+  //   center: tokyo,
+  //   zoom: 15
+  // });
 }
