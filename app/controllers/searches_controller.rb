445522d68ecfd6f004c29c18144b9e1e254c68c0
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-24 06:57:28 +0900

    修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,8 @@ class SearchesController < ApplicationController
   
   def post_search
     if params[:q][:artist_name_or_station_or_venue_or_address_cont].empty?
+      @search_post = Post.ransack(params[:q])
+      @posts = @search_post.result.order(date_time: "ASC").page(params[:page])
 
     else
       @search_post = Post.ransack(params[:q])
