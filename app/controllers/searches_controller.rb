abfd985e53b48b1f99d890eb710a3e4d807a5d1f
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-24 15:00:33 +0900

    修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,14 @@
 class SearchesController < ApplicationController
   
   def post_search
-    @search_post = Post.ransack(params[:q])
-    @posts = @search_post.result.order(date_time: "ASC").page(params[:page])
+    if params[:q][:status_in].present?
+      @search_post = Post.ransack(params[:q])
+      @posts = @search_post.result.order(date_time: "ASC").page(params[:page])
+
+    else
+        @search_post = Post.where(status: '1').ransack(params[:q])
+        @posts = @search_post.result.order(date_time: "ASC").page(params[:page])
+    end
   end
 
 
