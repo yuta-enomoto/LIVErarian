667523de12c32dfee27904a61c6c3680fa3b7dad
yuta-enomoto <yuta.eno04@gmail.com>
2020-03-26 21:08:55 +0900

    cssを設定

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,32 +1,61 @@
 function initMap() {
   if(!$("#map").length){
   }
+
   else {
-    'use strict';
+    if (window.location.href.match(/\/searches/)){
+      $('#map').css("height", "580px")
+      'use strict';
+      
+      var target = document.getElementById('map');
 
-    var target = document.getElementById('map');
-    var map;
-    var point = {lat: gon.latitude, lng: gon.longitude};
-    var name = gon.venue
-    var marker;
-    var infoWindow;
 
-    map = new google.maps.Map(target, {
-      center: point,
-      zoom: 15
-    });
+      if (!navigator.geolocation) {
+        alert('Geolocation not supported');
+        return;
+      }
 
-    marker = new google.maps.Marker({
-      position: point,
-      map: map,
-      animation: google.maps.Animation.DROP
-    });
+      navigator.geolocation.getCurrentPosition(function(position) {
+        new google.maps.Map(target, {
+          center: {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          },
+          zoom: 15
+        });
+      }, function() {
+        alert('Geolocation failed!');
+        return;
+      });
+    }
 
-    infoWindow = new google.maps.InfoWindow({
-      content: name
-    });
-    marker.addListener('click', function() { 
-      infoWindow.open(map, marker); 
-    });
+    else {
+      'use strict';
+
+      var target = document.getElementById('map');
+      var map;
+      var point = {lat: gon.latitude, lng: gon.longitude};
+      var name = gon.venue
+      var marker;
+      var infoWindow;
+
+      map = new google.maps.Map(target, {
+        center: point,
+        zoom: 15
+      });
+
+      marker = new google.maps.Marker({
+        position: point,
+        map: map,
+        animation: google.maps.Animation.DROP
+      });
+
+      infoWindow = new google.maps.InfoWindow({
+        content: name
+      });
+      marker.addListener('click', function() { 
+        infoWindow.open(map, marker); 
+      });
+    }
   }
 }
